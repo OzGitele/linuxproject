66b8a9dffb8ee49e4f95d163babd692871566c34
Oz <uzronen@microsoft.com>
2015-07-12 11:57:16 +0300

    changed port

--- mmofrontend/app.js
+++ mmofrontend/app.js
@@ -13,7 +13,7 @@ var api = require('./routes/webapi');
 
 
 // all environments
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
