e102f9c3d1890c2937e7063aecc52beae1cdb440
Oz <uzronen@microsoft.com>
2015-07-13 10:46:47 +0300

    sss

--- Script/templateScript.sh
+++ Script/templateScript.sh
@@ -1,34 +1,17 @@
-sudo apt-get update
+sudo apt-get -y update
 sudo apt-get -y upgrade
-
 sudo apt-get -y install nodejs
-
 sudo apt-get -y install npm
-
 sudo npm -y install express
-
 sudo npm -y install mongodb
-
-
 sudo npm -y install stylus
-
-
 sudo npm -y install jade
-
 sudo apt-get -y install git-core
-
-
 mkdir -p repos/linuxproject
-
 cd repos
-
 sudo git init
-
 sudo git clone https://github.com/OzGitele/linuxproject.git 
-
 cd linuxproject/mmofrontend
-
-
 sudo nodejs app.js
 
 
