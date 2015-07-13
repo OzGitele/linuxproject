00174f6e6c457f76431b062c5c75201eafc0f6a3
Oz <uzronen@microsoft.com>
2015-07-13 16:22:47 +0300

    ssss

--- Script/templateScript.sh
+++ Script/templateScript.sh
@@ -7,13 +7,13 @@ sudo npm -y install mongodb
 sudo npm -y install stylus
 sudo npm -y install jade
 sudo apt-get -y install git-core
-cd ~
-mkdir repos/linuxproject
+cd /
+sudo mkdir /repos
 cd repos
+sudo mkdir linuxproject
 sudo git init
 sudo git clone https://github.com/OzGitele/linuxproject.git 
 cd linuxproject/mmofrontend
-
 
 
 
